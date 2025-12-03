8a784aea648871e8ebf2e4632750c7a6afb0d176
Jirapan Yankhan <jirapan@mm.co.th>
2025-12-03 16:21:51 +0700

    fix: update type exports in index.ts for better clarity and consistency

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,21 @@ export * from "./components/LongdoTag";
 export * from "./utils/Overlays";
 export * from "./utils/Layer";
 export type {
-  Map as Map,
-  EventName as EventName,
+  Map,
+  OverlaysClickEvent,
+  EventName,
+  GeoJSONFeature,
+  CSSColor,
+  Geometry,
+  GeoJSONGeometry,
+  LineStyle,
+  Location,
+  LocationMode,
+  LongdoGlobal,
+  LongdoLayers,
+  LongdoMap,
+  LongdoMarkerProps,
   MarkerOptions,
   PopupOptions,
-  LongdoLayers as Layers,
+  Range
 } from "longdomap-type";
